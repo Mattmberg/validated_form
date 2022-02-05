a1527c9047f0c6c720c6f434de306c79fe31acbb
Matthew Bergeron Jr <84132501+Mattmberg@users.noreply.github.com>
2022-02-04 23:35:11 -0500

    Adding initial PHP file with HTML boiler plate

---
+++ data.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Validation Page</title>
+</head>
+<body>
+    <form method="post" action="<?php echo htmlspecialchars() ;?>"> 
+</body>
+</html>
